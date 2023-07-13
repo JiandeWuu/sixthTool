c5a914e366c099657f2a341dc633a8697dee5107
Jiande <jzxc56788@gmail.com>
2023-07-13 16:41:42 +0800

    0713

--- save_model.py
+++ save_model.py
@@ -17,8 +17,8 @@ from Function.ensemble_svm import ensemble_svm
 parser = argparse.ArgumentParser()
 parser.add_argument('-x', '--train_x', type=str, help='train_x file .npy')
 parser.add_argument('-y', '--train_y', type=str, help='train_y file .npy')
-parser.add_argument('-i', '--test_x', type=str, help='test_x file .npy')
-parser.add_argument('-l', '--test_y', type=str, help='test_y file .npy')
+parser.add_argument('-i', '--test_x', default=None, type=str, help='test_x file .npy')
+parser.add_argument('-l', '--test_y', default=None, type=str, help='test_y file .npy')
 # parser.add_argument('-o', '--output', default='output.png', type=str, help='output file')
 parser.add_argument('-m', '--method', default='svm', type=str, help='svm, libsvm, esvm')
 parser.add_argument('-p', '--hp', default='hyper_parameter.csv', type=str, help='hyper_parameter file')
@@ -30,10 +30,11 @@ print("train_x file: %s" % (args.train_x))
 x = np.load(args.train_x)
 print("train_y file: %s" % (args.train_y))
 y = np.load(args.train_y)
-print("test_x file: %s" % (args.test_x))
-test_x = np.load(args.test_x)
-print("test_y file: %s" % (args.test_y))
-test_y = np.load(args.test_y)
+if args.test_x and args.test_y:
+    print("test_x file: %s" % (args.test_x))
+    test_x = np.load(args.test_x)
+    print("test_y file: %s" % (args.test_y))
+    test_y = np.load(args.test_y)
 print("hyper_parameter file: %s" % (args.hp))
 if ".json" in args.hp:
     f = open(args.hp)
@@ -45,14 +46,16 @@ else:
 
 def esvm_train_model_hp_object(x, y, hp_object):
     clf = svm_function.esvm_train_model(x_train=x, y_train=y, 
+                                        classifier=hp_object['classifier'], 
                                         kernel=hp_object['kernel'], 
                                         C=hp_object['C'], 
-                                        logGamma=hp_object['logGamma'], 
+                                        gamma=hp_object['gamma'], 
                                         degree=hp_object['degree'], 
                                         coef0=hp_object['coef0'], 
-                                        n=hp_object['n'], 
+                                        nu=hp_object['nu'], 
                                         size=hp_object['size'], 
-                                        max_iter=hp_object['max_iter'])
+                                        max_iter=hp_object['max_iter'],
+                                        log=hp_object['log'])
     return clf
 
 def svm_train_model_hp_object(x, y, hp_object):
@@ -69,15 +72,17 @@ def svm_train_model_hp_object(x, y, hp_object):
 if args.method == 'svm':
     train_def = svm_train_model_hp_object
     model = train_def(x, y, hp)
-    decision_values = model.decision_function(test_x)
-    # decision_values = np.where(np.isfinite(decision_values), decision_values, 0) 
-    roc_score = metrics.roc_auc_score(test_y, decision_values)
-    print("AUROC: %.2f" % (roc_score))
+    if args.test_x and args.test_y:
+        decision_values = model.decision_function(test_x)
+        # decision_values = np.where(np.isfinite(decision_values), decision_values, 0) 
+        roc_score = metrics.roc_auc_score(test_y, decision_values)
+        print("AUROC: %.2f" % (roc_score))
 elif args.method == 'esvm':
     train_def = esvm_train_model_hp_object
     model = train_def(x, y, hp)
-    roc_score = model.test(test_x, test_y)
-    print("AUROC: %.2f" % (roc_score))
+    if args.test_x and args.test_y:
+        roc_score = model.test(test_x, test_y)
+        print("AUROC: %.2f" % (roc_score))
 
 print("save model file: %s" % (args.save))
 with open(args.save, 'wb') as f:
